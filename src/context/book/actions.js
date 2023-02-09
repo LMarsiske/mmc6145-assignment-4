4ff843ed88608f1aaa529157c534254c1c46cbfb
Lucas Marsiske <47642996+LMarsiske@users.noreply.github.com>
2023-02-08 21:50:44 -0500

    All tests passing

--- src/context/book/actions.js
+++ src/context/book/actions.js
@@ -1,1 +1,9 @@
 // TODO: Add and export actions: ADD_BOOK, REMOVE_BOOK, SEARCH_BOOKS
+
+const ADD_BOOK = "add"
+
+const SEARCH_BOOKS = "search"
+
+const REMOVE_BOOK = "remove"
+
+export { ADD_BOOK, SEARCH_BOOKS, REMOVE_BOOK }
--- src/context/book/reducer.js
+++ src/context/book/reducer.js
@@ -1,7 +1,27 @@
 // TODO: import actions and implement reducer for each action
-export default function reducer() {}
+import { ADD_BOOK, SEARCH_BOOKS, REMOVE_BOOK } from "./actions"
+
+export default function reducer(state, data) {
+  let favoriteBooks = null
+  switch (data.action) {
+    case ADD_BOOK:
+      favoriteBooks = [...state.favoriteBooks, data.payload]
+      saveToLocalStorage(favoriteBooks)
+      return { ...state, favoriteBooks }
+    case SEARCH_BOOKS:
+      return { ...state, bookSearchResults: data.payload }
+    case REMOVE_BOOK:
+      favoriteBooks = state.favoriteBooks.filter(
+        (book) => book.id !== data.payload
+      )
+      saveToLocalStorage(favoriteBooks)
+      return { ...state, favoriteBooks }
+    default:
+      return state
+  }
+}
 
 // This helper function stores the favoriteBook state in localStorage as a string
 function saveToLocalStorage(favBooks) {
-  localStorage.setItem('favoriteBooks', JSON.stringify(favBooks))
+  localStorage.setItem("favoriteBooks", JSON.stringify(favBooks))
 }
